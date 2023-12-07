2cb2acdc1ced8de46e998eb03e985f9dda362419
antareza <oneakun08@gmail.com>
2023-12-07 18:31:06 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        leaguegotic: ["leaguegotic"],
+        roboto: ["Roboto"],
+      },
+      colors: {
+        hitam: "#000000",
+        putih: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
